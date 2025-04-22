bfed719028633ad034cfe5da5c75fb597ca28d13
Haythem91 <haylive8@gmail.com>
2025-04-22 22:30:01 +0100

    setting up clerk & convex

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import  "bootstrap/dist/css/bootstrap.min.css";
+import {ClerkProvider} from "@clerk/nextjs";
+import {ConvexClientProvider} from "@/componants/ConvexClientProvider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +29,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+          <ConvexClientProvider>
+              <ClerkProvider>
+                  {children}
+              </ClerkProvider>
+          </ConvexClientProvider>
       </body>
     </html>
   );
