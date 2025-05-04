2b60bd08ee1be92056ed59055ac24feb5dd2e3cf
Haythem91 <haylive8@gmail.com>
2025-05-04 18:08:59 +0100

    added product card componant

--- componants/ProductSlider.tsx
+++ componants/ProductSlider.tsx
@@ -9,9 +9,9 @@ const ProductSlider = () => {
                 <div className="card">
                     <img src="https://eu.womensbest.com/cdn/shop/files/03_NewComfort_Linda_sage_1968_315x.jpg?v=1733851879" className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in
-                            to additional content. This content is a little bit longer.</p>
+                        <h5 className="card-title">Smooth Contour Cropped Short Sleeve T-Shirt</h5>
+                        <p className="card-text mb-1">Black</p>
+                        <p className={'card-text fw-medium'}>35$</p>
                     </div>
                 </div>
             </div>
@@ -19,9 +19,9 @@ const ProductSlider = () => {
                 <div className="card">
                     <img src="https://eu.womensbest.com/cdn/shop/files/04_NewDefine_Linda_468_315x.jpg?v=1728892321" className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in
-                            to additional content. This content is a little bit longer.</p>
+                        <h5 className="card-title">Smooth Contour Cropped Short Sleeve T-Shirt</h5>
+                        <p className="card-text mb-1">Black</p>
+                        <p className={'card-text fw-medium'}>35$</p>
                     </div>
                 </div>
             </div>
@@ -29,9 +29,9 @@ const ProductSlider = () => {
                 <div className="card">
                     <img src="https://eu.womensbest.com/cdn/shop/files/03_017_155_Look_315x.jpg?v=1745915865" className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in
-                            to additional content. This content is a little bit longer.</p>
+                        <h5 className="card-title">Smooth Contour Cropped Short Sleeve T-Shirt</h5>
+                        <p className="card-text mb-1">Black</p>
+                        <p className={'card-text fw-medium'}>35$</p>
                     </div>
                 </div>
             </div>
@@ -39,9 +39,9 @@ const ProductSlider = () => {
                 <div className="card">
                     <img src="https://eu.womensbest.com/cdn/shop/files/03_017_155_Look_315x.jpg?v=1745915865" className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in
-                            to additional content. This content is a little bit longer.</p>
+                        <h5 className="card-title">Smooth Contour Cropped Short Sleeve T-Shirt</h5>
+                        <p className="card-text mb-1">Black</p>
+                        <p className={'card-text fw-medium'}>35$</p>
                     </div>
                 </div>
             </div>
@@ -49,9 +49,9 @@ const ProductSlider = () => {
                 <div className="card">
                     <img src="https://eu.womensbest.com/cdn/shop/files/03_017_155_Look_315x.jpg?v=1745915865" className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in
-                            to additional content. This content is a little bit longer.</p>
+                        <h5 className="card-title">Smooth Contour Cropped Short Sleeve T-Shirt</h5>
+                        <p className="card-text mb-1">Black</p>
+                        <p className={'card-text fw-medium'}>35$</p>
                     </div>
                 </div>
             </div>
@@ -59,9 +59,9 @@ const ProductSlider = () => {
                 <div className="card">
                     <img src="https://eu.womensbest.com/cdn/shop/files/03_017_155_Look_315x.jpg?v=1745915865" className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in
-                            to additional content. This content is a little bit longer.</p>
+                        <h5 className="card-title">Smooth Contour Cropped Short Sleeve T-Shirt</h5>
+                        <p className="card-text mb-1">Black</p>
+                        <p className={'card-text fw-medium'}>35$</p>
                     </div>
                 </div>
             </div>
@@ -69,9 +69,9 @@ const ProductSlider = () => {
                 <div className="card">
                     <img src="https://eu.womensbest.com/cdn/shop/files/03_017_155_Look_315x.jpg?v=1745915865" className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in
-                            to additional content.</p>
+                        <h5 className="card-title">Smooth Contour Cropped Short Sleeve T-Shirt</h5>
+                        <p className="card-text mb-1">Black</p>
+                        <p className={'card-text fw-medium'}>35$</p>
                     </div>
                 </div>
             </div>
@@ -79,9 +79,9 @@ const ProductSlider = () => {
                 <div className="card">
                     <img src="https://eu.womensbest.com/cdn/shop/files/03_017_155_Look_315x.jpg?v=1745915865" className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in
-                            to additional content. This content is a little bit longer.</p>
+                        <h5 className="card-title">Smooth Contour Cropped Short Sleeve T-Shirt</h5>
+                        <p className="card-text mb-1">Black</p>
+                        <p className={'card-text fw-medium'}>35$</p>
                     </div>
                 </div>
             </div>
