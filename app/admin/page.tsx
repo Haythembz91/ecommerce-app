d9c1f896b92eee719b89c1f5ac23f8cb6b963ba7
Haythembz91 <154563842+Haythembz91@users.noreply.github.com>
2025-05-11 10:29:08 +0000

    Coding api

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,11 @@
 import { auth } from '@clerk/nextjs/server'
 import DashboardForm from "@/componants/DashboardForm";
-import Users from "@/componants/Users";
+
 
 export default async function Home() {
   const { userId } = await auth.protect({ permission: 'org:dashboard:admin' })
 
   return (
-      <Users></Users>
+      <DashboardForm/>
   )
 }
