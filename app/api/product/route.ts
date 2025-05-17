abf7118081e61e036db5312e7fef2ed889b066ea
Haythembz91 <154563842+Haythembz91@users.noreply.github.com>
2025-05-17 08:59:17 +0000

    Added products done

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,13 +1,17 @@
 import {NextResponse, NextRequest} from "next/server";
-import { getDb } from "../../../utils/mongodb";
+import { getDb } from "@/utils/mongodb";
 
 export async function POST(req:NextRequest){
     try {
-        const body = await req.json()
-        console.log(body)
-        return NextResponse.json({message:'Product Created Successfully'}, {status: 201})
+        const product = await req.json()
+        console.log(product)
+        const db=await getDb()
+        const productsCollection = db.collection('products')
+        const addProduct = await productsCollection.insertOne(product)
+        if(addProduct.acknowledged){
+            return NextResponse.json({message:'Product Created Successfully'}, {status: 201})
+        }
     } catch (error) {
         console.error(error)
     }
-    
 }
