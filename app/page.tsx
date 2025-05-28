632c462012f42ce248988718612f3428ea191325
Haythembz91 <154563842+Haythembz91@users.noreply.github.com>
2025-05-28 18:48:51 +0000

    Some UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import SliderSportswear from "@/componants/SliderSportswear"
-import ProductSlider from "@/componants/ProductSlider";
 import Link from "next/link";
 import {routes} from "@/utils/enums";
+import mobileBanner from "@/1.WB_LowBackEdit_LaunchBanner_1600x2100_20250520_dp_mobile_400x@2x.jpg"
 const Home = async ()=>{
 
-    console.log("Ania the best")
+    console.log("Ania Kubow")
     
     return(
         <div>
@@ -17,6 +17,22 @@ const Home = async ()=>{
                     <Link className={'fw-medium link-dark link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover'} href={`/shop${routes.SPORTSWEAR}`}>Shop</Link>
                 </div>
                 
+            </section>
+            <section className={'my-2 mx-md-2'}>
+                <div className="card text-bg-dark">
+                  <img src={mobileBanner.src} className="card-img" alt="..."/>
+                  <div className="carousel-caption">
+                    <h3 className="card-title fw-bold ">LOW BACK EDIT</h3>
+                    <h6 className="card-text">Bringing the heat.</h6>
+                      <button className={"btn btn-light"}
+                              style={{
+                                  fontWeight: "600",
+                                  borderRadius: "20px",
+                              }}>
+                          Shop now
+                      </button>
+                  </div>
+                </div>
             </section>
         </div>
     )
