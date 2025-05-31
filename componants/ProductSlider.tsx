dd5090d9646e54b95559778cbc58e50b9754331f
Haythembz91 <154563842+Haythembz91@users.noreply.github.com>
2025-05-31 11:38:41 +0000

    cleanup slider

--- componants/ProductSlider.tsx
+++ componants/ProductSlider.tsx
@@ -1,13 +1,12 @@
 import { GetProducts } from "@/utils/GetProducts"
 import { Product } from "@/utils/interfaces"
-import ProductsContainer from "./ProductsContainer"
 import { collections } from "@/utils/enums"
 import ProductCard from "./ProductCard"
 
 
 
 const ProductSlider = async () => {
-    const favoriteProducts :Product[]= await GetProducts({productCollection:collections.DEFINE})
+    const favoriteProducts :Product[]= await GetProducts({productCollection:collections.SMOOTHCONTOUR})
     return(
         <div className="row row-cols-2 gx-2 flex-nowrap overflow-x-scroll productSlide">
             {favoriteProducts.map((product,index)=>
