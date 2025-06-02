4b0af4e57f77162446b3f6b31be591dd30db9bfc
Haythembz91 <154563842+Haythembz91@users.noreply.github.com>
2025-06-02 20:34:49 +0000

    sorting is done

--- app/shop/sportswear/page.tsx
+++ app/shop/sportswear/page.tsx
@@ -1,14 +1,19 @@
 import {Product} from "@/utils/interfaces";
 import {GetProducts} from "@/utils/GetProducts";
 import ProductsContainer from "@/componants/ProductsContainer"
+import {sort} from "@/utils/enums"
 
 const Home = async ({searchParams})=>{
     
     const filters = await searchParams
     const sortOrder = filters.sort
     delete filters.sort
-    console.log(filters)
     const products:Product[] = await GetProducts(filters)
+    if (sortOrder===sort.PRICE_ASC){
+        products.sort((a,b)=>a.productPrice-b.productPrice)
+    } else if (sortOrder===sort.PRICE_DSC){
+        products.sort((a,b)=>b.productPrice-a.productPrice)
+    }else products.sort((a,b)=>new Date(b.dateAdded)- new Date(a.dateAdded))
      
     return(
        <ProductsContainer products={products}/>           
