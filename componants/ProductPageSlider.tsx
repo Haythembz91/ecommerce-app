612b29ba801c7b3e905a08ebf84b57e61bcd13a4
Haythem91 <haylive8@gmail.com>
2025-06-07 17:08:13 +0100

    product page carousel

--- componants/ProductPageSlider.tsx
+++ componants/ProductPageSlider.tsx
@@ -24,8 +24,9 @@ const ProductPageSlider = ({product}: { product:Product })=>{
             </div>
             <div className="carousel-indicators position-absolute top-100">
                 {product.urlByColor.map((url,index)=>(
-                    <button key={index} type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={index} style={{backgroundImage:`url(${url})`,height:'20vh',maxHeight:'120px',backgroundSize:'cover'}}
-                            className={index === 0 ? 'active w-100' : 'w-100'} aria-current={index === 0 ? 'true' : 'false'} aria-label={`Slide ${index+1}`}>
+                    <button key={index} type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={index}
+                            className={index === 0 ? 'active' : ''} style={{width:'200px'}} aria-current={index === 0 ? 'true' : 'false'} aria-label={`Slide ${index+1}`}>
+                        <img src={url} className="d-block w-100" alt="product image"/>
                     </button>
                 ))}
             </div>
