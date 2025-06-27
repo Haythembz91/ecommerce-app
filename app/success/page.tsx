cc77bc56e1f5c815e84dac78258cbf3606d28be7
Haythem91 <haylive8@gmail.com>
2025-06-27 03:25:42 +0100

    stripe webhook + saving to mongodb ts fixes

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -5,10 +5,6 @@ export default async function Success({ searchParams }:{searchParams:Promise<{se
     const { session_id } = await searchParams
     if (!session_id)
         throw new Error('Please provide a valid session_id (`cs_test_...`)')
-
-    const session = await stripe.checkout.sessions.retrieve(session_id,{
-        expand: ['line_items', 'payment_intent']
-    })
     const {
         status,
         customer_details
