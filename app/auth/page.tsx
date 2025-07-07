899cf82c305724a65a619194c9f2dea325833029
Haythem91 <haylive8@gmail.com>
2025-07-07 18:49:10 +0100

    fixing middleware.ts

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,12 +1,11 @@
 import AuthComponent from "@/components/AuthComponent";
-import banner from '@/public/assets/Gemini_Generated_Image_ezh7coezh7coezh7.png'
 
 
 const Home = ()=>{
     return (
         <div className={'d-md-flex bg-body-secondary'}>
             <div className={'col-md-7'}>
-                <img src={banner.src} className={'img-fluid'}/>
+                <img alt={'auth-banner'} src={'https://res.cloudinary.com/dmgfsayir/image/upload/v1751910196/Gemini_Generated_Image_ezh7coezh7coezh7_g6h9or.png'} className={'img-fluid'}/>
             </div>
             <AuthComponent></AuthComponent>
         </div>
